cae2beeda6d2004cf5fee47989de7a2991f6e099
suganthsuji <suganthpv.007@gmail.com>
2022-08-08 19:32:41 +0530

    Added: Invoice Service for CabInvoice

--- src/test/java/com/test/CabInvoiceTest.java
+++ src/test/java/com/test/CabInvoiceTest.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import com.cabinvoice.InvoiceGenerator;
 import com.cabinvoice.cabInvoice;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,6 +27,23 @@ public class CabInvoiceTest {
         Double result=cabinvoice.calculateTotalFare(rideArray);
         Assert.assertEquals(233.5,result,0);
 
+    }
+    //To Calculate Number of Ride and Average Fare Price
+    @Test
+    public void givenArrayWhenCalculateShouldReturnNumberOfRidesTotalFareAndAverageFarePrice()
+    {
+        InvoiceGenerator invoice = new InvoiceGenerator();
+        double[][] rideArray = {
+                {5,20},
+                {2,0.5},
+                {1.5,2}
+        };
+        int noOfRides = invoice.getTotalRides(rideArray);
+        double totalFare = invoice.calculateTotalFare(rideArray);
+        double averageFare = invoice.calculateAverageFare(rideArray);
+        Assert.assertEquals(3,noOfRides);
+        Assert.assertEquals(165,totalFare,0);
+        Assert.assertEquals(55,averageFare,0);
     }
 
 }
